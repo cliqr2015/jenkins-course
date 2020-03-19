b4c52a2c23c9f96f1b15864fcba40f5326ca78cb
cliqr2015 <cliqr2015@gmail.com>
2020-03-18 20:15:02 -0700

    m18-03

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/cliqr2015/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/cliqr2015/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
